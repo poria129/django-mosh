39311c261e084fd6c97474df8d8218c1dcaf5059
poria129 <89725449+poria129@users.noreply.github.com>
2023-08-04 19:26:22 +0430

    Serializer to ModelSerializer
    1. change the Serializer to ModelSerializer
    2. make a collection view and url

--- store/views.py
+++ store/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related("collection").all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data)
 
@@ -16,4 +16,11 @@ def product_list(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    queryset = Product.objects.all()
+    serializer = CollectionSerializer(queryset, many=True)
     return Response(serializer.data)
