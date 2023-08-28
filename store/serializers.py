d3cb0e69ac5be6b780e1043994d9b3ad423ffac6
poria129 <89725449+poria129@users.noreply.github.com>
2023-08-28 11:29:01 +0430

    getting a cart
    - added abiliti to get a cart with two extra fields :
    1- product price
    2- total price

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from rest_framework import serializers
 
-from .models import Product, Collection, Review, Cart
+from .models import CartItem, Product, Collection, Review, Cart
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -45,9 +45,36 @@ class ReviewSerializer(serializers.ModelSerializer):
         return Review.objects.create(product_id=product_id, **validated_data)
 
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    product_price = serializers.SerializerMethodField(
+        method_name="product_calculate_price"
+    )
+
+    def product_calculate_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.unit_price
+
+    class Meta:
+        model = CartItem
+        fields = ["product", "quantity", "product_price"]
+
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name="calculate_total_price")
+
+    def calculate_total_price(self, cart: Cart):
+        return sum(
+            [item.quantity * item.product.unit_price for item in cart.items.all()]
+        )
 
     class Meta:
         model = Cart
-        fields = ["id"]
+        fields = ["id", "items", "total_price"]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
@@ -57,6 +57,6 @@ class ReviewViewSet(ModelViewSet):
         return {"product_id": self.kwargs["product_pk"]}
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related("items__product").all()
     serializer_class = CartSerializer
