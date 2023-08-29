d7a65c853830f62d1b5dbfb03057992d633d729c
poria129 <89725449+poria129@users.noreply.github.com>
2023-08-29 11:30:22 +0430

    Cart & CartItem functions
    adding:
    1- delete a cart
    2- get cart items
    3- adding a cart item
    4- updating a cart Item

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,10 @@ router.register("products", views.ProductViewSet)
 router.register("collections", views.CollectionViewSet)
 router.register("carts", views.CartViewSet)
 
-product_router = routers.NestedDefaultRouter(router, "products", lookup="product")
-product_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
+products_router = routers.NestedDefaultRouter(router, "products", lookup="product")
+products_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
 
-urlpatterns = router.urls + product_router.urls
+carts_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
+carts_router.register("items", views.CartItemViewSet, basename="cart_items")
+
+urlpatterns = router.urls + products_router.urls + carts_router.urls
